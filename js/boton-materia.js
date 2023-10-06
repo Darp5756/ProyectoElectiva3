884601b5eab4fd8fc278856fa6748f073099a07a
Darp5756 <darp7512@gmail.com>
2023-10-06 01:16:43 -0400

    first commit

---
+++ js/boton-materia.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $('.boton-materia').on('click', function() {
+
+        let materia = $(this).text();
+        let imagen = datos_materias[materia]['imagen'];
+        let autor = datos_materias[materia]['autor'];
+
+        //Cambiar datos del motal
+        $('#titulo-materia').text(materia);
+        $('#imagen-materia').attr('src', 'img/' + (imagen || 'sin_imagen.png'));
+        $('#imagen-de').text('De: ' + (autor || 'Nadie todavía'));
+
+    });
+
+});
---
+++ js/datos-materias.js
@@ -0,0 +1,266 @@
+const datos_materias = {
+    "Matemática General": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Introducción a la Ingeniería": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Lengua y Comunicación": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Computación Básica": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Lógica": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Formación Deportiva": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Cálculo Diferencial": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Álgebra Lineal": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Geometría": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Inglés Instrumental": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Metodología de Estudio": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Educación Ambiental": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Cálculo Integral": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Algoritmo, Estructura y Programación": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Mecánica": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Inglés Técnico": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Estadística Descriptiva y Probabilidad": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Actividades Recreacionales": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Cálculo de Varias Variables": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Estructuras Discretas": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Electricidad y Magnetismo": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Redacción de Informes": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Estadística Inferencial": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Química Aplicada": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Ecuaciones Diferenciales": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Programación Estructurada": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Laboratorio de Mecánica": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Base de Datos": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Creatividad": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Orientación Personal": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Investigación de Operaciones I": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Programación Orientada a Objeto": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Laboratorio de Electricidad y Magnetismo": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Laboratorio de Base de Datos": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Análisis de Sistemas de Información": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Sistemas de Control": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Investigación de Operaciones II": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Sistemas Digitales": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Introducción a las Redes": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Electiva I - Valores y Cultura Ciudadana": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Diseño de Sistemas de Información": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Prácticas Profesionales I": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Servicio Comunitario": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Ingeniería Económica": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Aplicaciones Web": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Arquitectura del Computador": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Innovación": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Implementación de Sistemas de Información": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Orientación Profesional": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Auditoría de Sistemas": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Sistemas de Información Gerenciales": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Sistemas Operativos": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Emprendimiento": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Ingeniería del Software": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Electiva II - Técnicas y Desarrollo Web": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Seguridad Informática": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Sistemas Expertos": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Gerencia de Sistemas": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Electiva III": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Formulación y Evaluación de Proyectos": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Ética Aplicada y Política": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Calidad de los Sistemas": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Ejecución y Control de Proyectos": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Prácticas Profesionales II": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Trabajo Especial de Titulación": {
+        "imagen": "",
+        "autor": ""
+    },
+    "Prácticas Profesionales III": {
+        "imagen": "",
+        "autor": ""
+    }
+};
