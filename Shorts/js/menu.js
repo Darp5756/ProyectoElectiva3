a2948acc44abd886071da000ff48cf6edba77b17
DarkOver20 <anibalmorillo1@gmail.com>
2023-11-11 18:46:20 -0400

    hola

---
+++ Shorts/js/menu.js
@@ -0,0 +1,16 @@
+ // Para poder desplegarse el menu
+
+const menu = document.querySelector("#menu");
+const menuBtns = document.querySelectorAll(".menu-btn");
+
+menuBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    menu.classList.toggle("visible");
+  });
+});
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !menuBtns[0].contains(e.target)) {
+    menu.classList.remove("visible");
+  }
+});
