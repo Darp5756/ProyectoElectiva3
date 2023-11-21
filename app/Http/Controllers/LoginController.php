d25a75e696d800e48104d5fd2c21ceb6448d9718
LuisMc28 <lmedinacabrera28@gmail.com>
2023-11-21 18:44:12 -0430

    blade

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,7 +11,7 @@ class LoginController extends Controller
 {
     public function Vista_Login ()
     {
-        return view('');
+        return view('sesion.blade.php');
     }
 
     public function Login(Request $request)
