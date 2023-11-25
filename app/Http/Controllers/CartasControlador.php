c51caf105fbb0cc74f665f73862c470f6bddc9a0
LuisMc28 <lmedinacabrera28@gmail.com>
2023-11-25 18:31:22 -0430

    aaaa

---
+++ app/Http/Controllers/CartasControlador.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartasControlador extends Controller
+{
+    public function Vista_Registro ()
+    {
+        return view('card');
+    }
+}
