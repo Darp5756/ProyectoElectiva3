c910b193688c88303863173731e291435966e584
DarkOver20 <anibalmorillo1@gmail.com>
2023-11-25 19:59:52 -0400

    as

--- resources/views/short.blade.php
+++ resources/views/short.blade.php
@@ -82,75 +82,47 @@
       </nav>
  <!-- Slider   -->
      
-        <div class="slider">
-          <div class="controls">
-            <div class="up">
-              <i class="fa fa-chevron-up"></i>
-            </div>
-            <div class="down">
-              <i class="fa fa-chevron-down"></i>
-            </div>
-          </div>
-          <!--Titulo y descepcion parte izquierda-->
-          <div class="wrapper">
-            <div class="left">
-              <div>
-                <h2>Ingenieria en sistemas</h2>
+ <div class="slider">
+      <div class="controls">
+        <div class="up">
+          <i class="fa fa-chevron-up"></i>
+        </div>
+        <div class="down">
+          <i class="fa fa-chevron-down"></i>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <div class="left">
+          <div>
+          <h2>Ingenieria en sistemas</h2>
                 <p>
                     La ingeniería en sistemas se enfoca en el diseño, desarrollo, implementación y mantenimiento de sistemas complejos, incluyendo hardware, software y servicios. 
                     Los ingenieros en sistemas se desempeñan en diversas industrias como tecnología, salud, biología, economía, comunicaciones y más. Algunas posiciones comunes son director de tecnología de la información, desarrollador de software, administrador de proyectos y arquitecto de sistemas.
                 </p>
-              </div>
-    
-              <div>
-                <h2>Inteligencia artificial</h2>
-                <p>
-                  Es un campo de estudio y desarrollo de tecnologías que buscan crear sistemas y programas capaces de realizar tareas que normalmente requieren de la inteligencia humana. Estos sistemas pueden aprender, razonar, reconocer patrones y tomar decisiones basadas en datos.
-                  <br><br>En el campo de la ingeniería en sistemas, la IA tiene diversas aplicaciones y 
-                  beneficios. Algunos ejemplos incluyen: Ciberseguridad, Automatización de procesos, Sistemas expertos, 
-                  Internet de las cosas (IoT), Optimización de sistemas, Desarrollo web, etc.
-                  
-                </p>
-              </div>
-  
-              <div>
-                <h2>Robotica</h2>
-                <p>
-                  Es un campo que combina la ingeniería y la tecnología para diseñar, desarrollar y 
-                  utilizar robots en diversos entornos y aplicaciones. Los robots son máquinas programables capaces de realizar 
-                  tareas de manera autónoma o semiautónoma.<br><br>
-                  El papel de un ingeniero en sistemas en la robótica es fundamental para el desarrollo, implementación y 
-                  mantenimiento de sistemas robóticos. Los ingenieros en sistemas aplican sus conocimientos en áreas como la electrónica,
-                   la informática y la ingeniería de control para diseñar y programar robots que puedan realizar tareas de manera autónoma o semiautónoma.
-                </p>
-              </div>
-    
-            </div>
-            <!--Videos parte derecha-->
-            <div class="right">
-              
-                  <div>
-                    <video class="right miVideo" controls autoplay muted>
-                      <source src="../video/robotica.mp4" type="video/mp4">
-                    </video>
-                  </div>
+          </div>
+        </div>
+        <div class="right">
+          <div>
+          </div>
 
-                  <div>
-                    <video class="right miVideo" controls autoplay muted>
-                      <source src="../video/inteligencia.mp4" type="video/mp4">
-                    </video>
-                  </div>
+          <div>
+          </div>
 
-                  <div>
+          <div>
+          </div>
+
+          <div>
+          </div>
+
+          <div>
                     <video class="right miVideo" controls autoplay muted>
                       <source src="../video/sistemas.mp4" type="video/mp4">
                     </video>
                   </div>
-
-                  
-            </div>
-          </div>
         </div>
+      </div>
+    </div>
 
     <!-- Llamado a js -->
     <script src="{{asset('js/slider.js')}}"></script>
