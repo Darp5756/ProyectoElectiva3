7d0f9770ddcd9c719cff5f961a0d4db29a2418ca
DarkOver20 <anibalmorillo1@gmail.com>
2023-11-25 18:27:08 -0400

    asdas

---
+++ resources/views/Card.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title> Cursos</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<body>
+   
+<div class="container">
+	<div class="blob-c">
+    <div class="shape-blob"></div>
+    <div class="shape-blob one"></div>
+    <div class="shape-blob two"></div>
+    <div class="shape-blob three"></div>
+    <div class="shape-blob four"></div>
+    <div class="shape-blob five"></div>
+    <div class="shape-blob six"></div>
+  </div>
+
+  <section>
+    <div class="card">
+      <img src="{{ asset('img/robotica.jpg') }}" alt="image" />
+
+      <div class="card-content">
+        <p>Robotica</p>
+        <p>La robótica es una ciencia que se ocupa del diseño, construcción y aplicación de los robots, que son
+                máquinas automáticas con cierto grado de inteligencia.</p>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="{{ asset('img/IA.jpg') }}" alt="image" />
+
+      <div class="card-content">
+        <p>Inteligencia artificial</p>
+        <p>La inteligencia artificial (IA) es la capacidad de las máquinas de realizar tareas que normalmente requieren inteligencia humana, como el reconocimiento de imágenes, el procesamiento del lenguaje natural o la toma de decisiones.</p>
+      </div>
+    </div>
+  </section>
+</div>
+
+</body>
+<!-- partial -->
+
+</body>
+</html>
--- resources/views/Robotica.blade.php
+++ resources/views/Robotica.blade.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" 
       integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous"
       referrerpolicy="no-referrer" />
-
   </head>
   <body>
        <!-- Menu -->
@@ -43,12 +42,9 @@
           <span>Robotica</span>
         </div>
         <div class="menu-item">
-  <a class="enlance" href="/resources/views/Card.blade.php">
-    <img src="../svg/curso.svg">
-    <span>Cursos</span>
-  </a>
-</div>
-
+          <img src="../svg/curso.svg">
+          <span>Cursos</span>
+        </div>
       </div>
 
       <div class="menu-group">
@@ -85,28 +81,9 @@
           <div class="wrapper">
             <div class="left">
               <div>
-                <h2>Ingenieria en sistemas</h2>
-                <p>
-                    La ingeniería en sistemas se enfoca en el diseño, desarrollo, implementación y mantenimiento de sistemas complejos, incluyendo hardware, software y servicios. 
-                    Los ingenieros en sistemas se desempeñan en diversas industrias como tecnología, salud, biología, economía, comunicaciones y más. Algunas posiciones comunes son director de tecnología de la información, desarrollador de software, administrador de proyectos y arquitecto de sistemas.
-                </p>
-              </div>
-    
-              <div>
-                <h2>Inteligencia artificial</h2>
-                <p>
-                  Es un campo de estudio y desarrollo de tecnologías que buscan crear sistemas y programas capaces de realizar tareas que normalmente requieren de la inteligencia humana. Estos sistemas pueden aprender, razonar, reconocer patrones y tomar decisiones basadas en datos.
-                  <br><br>En el campo de la ingeniería en sistemas, la IA tiene diversas aplicaciones y 
-                  beneficios. Algunos ejemplos incluyen: Ciberseguridad, Automatización de procesos, Sistemas expertos, 
-                  Internet de las cosas (IoT), Optimización de sistemas, Desarrollo web, etc.
-                  
-                </p>
-              </div>
-  
-              <div>
                 <h2>Robotica</h2>
                 <p>
-                  Es un campo que combina la ingeniería y la tecnología para diseñar, desarrollar y 
+                Es un campo que combina la ingeniería y la tecnología para diseñar, desarrollar y 
                   utilizar robots en diversos entornos y aplicaciones. Los robots son máquinas programables capaces de realizar 
                   tareas de manera autónoma o semiautónoma.<br><br>
                   El papel de un ingeniero en sistemas en la robótica es fundamental para el desarrollo, implementación y 
@@ -116,16 +93,18 @@
               </div>
     
               <div>
-                <h2>Programacion</h2>
+                <h2></h2>
                 <p>
-                  La programación es el proceso de crear un conjunto de instrucciones ordenadas que le indican a una computadora cómo 
-                  realizar una tarea específica. En el campo de la ingeniería de sistemas, 
-                  los ingenieros utilizan la programación para desarrollar software, automatizar tareas y resolver problemas.
-                  Un ingeniero en sistemas utiliza sus conocimientos en programación para diseñar y desarrollar software, 
-                  ya sea para aplicaciones móviles, sistemas web, sistemas de control, inteligencia artificial, entre otros. 
-                  También se encarga de optimizar el rendimiento de los sistemas, solucionar problemas y mantener el software actualizado.
+                  
                 </p>
               </div>
+  
+              <div>
+                <h2></h2>
+                <p>
+                 </p>
+              </div>
+
             </div>
             <!--Videos parte derecha-->
             <div class="right">
